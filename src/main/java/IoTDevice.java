2ac896182c875bb60d1536de36434fb96954f537
natepy <bfnoc99@gmail.com>
2023-04-06 17:42:29 +0100

    circleci added

--- src/main/java/IoTDevice.java
+++ src/main/java/IoTDevice.java
@@ -2,21 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class IoTDevice {
-
     private List<Double> readings;
 
     public IoTDevice() {
         readings = new ArrayList<>();
     }
-
     public void saveReading(Double reading) {
         readings.add(reading);
     }
-
     public void clearReadings() {
         readings.clear();
     }
-
     public String formatReadings() {
         StringBuilder format = new StringBuilder("{ 'reported': [ ");
         for (Double reading : readings) {
@@ -25,7 +21,6 @@ public class IoTDevice {
         format.append(" ] }");
         return format.toString();
     }
-
     public String formatAverageReading() {
         String format = "{ 'average': ";
         Double total = 0.0;
@@ -35,5 +30,4 @@ public class IoTDevice {
         format += total / readings.size() + " }";
         return format;
     }
-
 }
